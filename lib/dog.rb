26fa03dcc0d6dc029c7d2a826be806e581a139d3
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2017-10-15 21:30:34 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,21 +1,9 @@
-def Dog
-  @name
-  @breed
-
-def name
-  @name
-end
-
-def name=(new_name)
-  name = new_name
-end
-
-def breed
-  @breed
-end
-
-def breed=(new_breed)
-  breed = new_breed
-end
-
+class Dog
+  def name=(dog_name)
+    this_dogs_name = dog_name
+  end
+ 
+  def name
+    this_dogs_name
+  end
 end
