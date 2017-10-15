f84af3e3dc7e835c1d75a2475cc0fcad298e3db0
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2017-10-15 21:56:00 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -6,4 +6,16 @@ class Person
   def name
     @name
   end
+  def name=(new_name)
+    @name = new_name
+  end
+
+  def breed
+    @breed
+  end
+
+  def breed=(new_breed)
+    @breed= new_breed
+  end
+  
 end
